5877355d67fba5d4ec4b9ab151e5acb3f32895d6
Arunkumar-CAPE <78420626+Arunkumar-CAPE@users.noreply.github.com>
2022-06-08 12:38:00 +0530

    Revert "added exception class in global exception handler"
    This reverts commit b5ea595b95c1d3e01ab21a0cdae33787a1ad4c40.

--- src/main/java/com/capeelectric/exception/GlobalExceptionHandler.java
+++ src/main/java/com/capeelectric/exception/GlobalExceptionHandler.java
@@ -23,20 +23,8 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<ErrorMessage>(exceptionMessage, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
 	}
 	
-	@ExceptionHandler({ MCCBException.class })
-	public ResponseEntity<ErrorMessage> handleMCCBException(MCCBException e) {
-		ErrorMessage exceptionMessage = new ErrorMessage(e.getMessage(), e.getLocalizedMessage(), "406");
-		return new ResponseEntity<ErrorMessage>(exceptionMessage, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
-	}
-	
 	@ExceptionHandler({ RCBOException.class })
 	public ResponseEntity<ErrorMessage> handleRCBOException(RCBOException e) {
-		ErrorMessage exceptionMessage = new ErrorMessage(e.getMessage(), e.getLocalizedMessage(), "406");
-		return new ResponseEntity<ErrorMessage>(exceptionMessage, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
-	}
-	
-	@ExceptionHandler({ LightException.class })
-	public ResponseEntity<ErrorMessage> handleLightException(LightException e) {
 		ErrorMessage exceptionMessage = new ErrorMessage(e.getMessage(), e.getLocalizedMessage(), "406");
 		return new ResponseEntity<ErrorMessage>(exceptionMessage, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
 	}
